b7d6707a6859bf06209e1acbe50de79668bfaff6
Aamir <aamirsaras@gmail.com>
2024-03-30 13:38:31 +0530

    updated

--- terraform/aws/resources/radical-bastion/vars.tf
+++ terraform/aws/resources/radical-bastion/vars.tf
@@ -1,39 +1,32 @@
 variable "AWS_ACCESS_KEY" {
-	default = "AKIA2RJEWVSXY23ANAFR"
+	default = "AKIA23DKME3MZ3VOHR5G"
 }
 
 variable "AWS_SECRET_KEY" {
-	default = "nAPEfUyo53ty8jkNbsiYUuuMG8gCDCJd5HexHGFi"
+	default = "HPyHrp6n0O8fm1ZtCR+gNLY7lBVYAoInSc676F29"
 }
 
 variable "mykey" {
 	type = string
-	default = "radicalsep2023"
+	default = "radicaldec2023"
 	
 }
-
-variable "subnet" {
-	default = "us-west-2a"
-}
-
-variable "security-group" {
-	type = map(list(string))
-	default = {
-		"mysg" = ["sg-0342ba743b97deee2"]
-		
-	}
-}
-
 
 variable "keyPath" {
    default = "aws.pem"
 }
 
+variable "subnet" {
+	default = "us-west-2b"
+}
+
 variable "subnets" {
 	type = map(string)
 	default = {
-		us-west-2a = "subnet-0656b8e19940c8b8f"
-		us-west-2b = "subnet-0a5bad7d52c80e5f5"
+		us-west-2a = "subnet-099e7de53fcebf8da"
+        us-west-2b = "subnet-0a8c1504f09e1828f"
+        us-west-2c = "subnet-04518523cc7b9dcf9"
+        us-west-2d = "subnet-01095ab50bf229d74"
 	}
 }
 
@@ -42,12 +35,23 @@ variable "AWS_REGION" {
   default = "us-west-2"
 }
 
+variable "security-group" {
+	type = map(list(string))
+	default = {
+		"mysg" = ["sg-0469f3af6e8d9926a"]
+		
+	}
+}
+
 variable "AMIS" {
   type = map(string)
   default = {
-    us-east-1 = "ami-00c6177f250e07ec1"
-    us-west-2 = "ami-01450e8988a4e7f44"
+    us-east-1 = "ami-13be557e"
+    us-west-2 = "ami-00112c992a47ba871"
     eu-west-1 = "ami-0d729a60"
-    us-east-2 = "ami-0fa49cc9dc8d62c84"
+	us-east-2 = "ami-0fa49cc9dc8d62c84"
   }
+
 }
+
+
