7e87257d9deb04467301feb7d8bc9dbcce6bd835
Aamir <aamirsaras@gmail.com>
2024-04-14 12:12:29 +0530

    updated

--- deploy_script.sh
+++ deploy_script.sh
@@ -1,13 +1,13 @@
 #/bin/bash
 
 echo 'Deployment..'
-sh 'sudo yum install httpd -y'
-sh 'sudo yum install elinks -y'
-sh 'sudo systemctl start httpd'
-sh 'sudo systemctl enable httpd'
-sh 'sudo rm -rf /var/www/html/*'
-sh 'sudo rsync -avt webapp/target/webapp /var/www/html'
-sh 'sudo elinks  http://${Node_IP}/webapp/'
-sh 'sudo elinks  http://${Node_IP}/webapp/index_dev.jsp'
-sh 'sudo elinks  http://${Node_IP}/webapp/index.html'
-sh 'sudo curl -kv http://${Node_IP}/webapp/index_dev.jsp'
+sudo yum install httpd -y
+sudo yum install elinks -y
+sudo systemctl start httpd
+sudo systemctl enable httpd
+sudo rm -rf /var/www/html/*
+sudo rsync -avt webapp/target/webapp /var/www/html
+sudo elinks  http://${Node_IP}/webapp/
+sudo elinks  http://${Node_IP}/webapp/index_dev.jsp
+sudo elinks  http://${Node_IP}/webapp/index.html
+sudo curl -kv http://${Node_IP}/webapp/index_dev.jsp
